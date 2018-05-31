daadecb459166fa1b732cf68f25e1167ef1352d0
Joe Ortega <CoderBear@users.noreply.github.com>
2018-05-31 13:37:16 +0930

    0.0.002
    Added physics and made the ground and platform immovable and not prone to the forces of gravity.

--- js/main.js
+++ js/main.js
@@ -7,10 +7,15 @@ var GameState = {
     //execute after everything is loaded
     create: create,
 };
+
+var physics;
 function init() {
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
+
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.game.physics.arcade.gravity.y = 1000;
 }
 
 function preload() {
@@ -27,12 +32,19 @@ function preload() {
 
 function create() {
     this.ground = this.add.sprite(0, 500, 'ground');
+    this.game.physics.arcade.enable(this.ground);
+    this.ground.body.allowGravity = false;
+    this.ground.body.immovable = true;
     
     var platform = this.add.sprite(0, 300, 'platform');
+    this.game.physics.arcade.enable(platform);
+    platform.body.allowGravity = false;
+    platform.body.immovable = true;
     
     this.player = this.add.sprite(100, 200, 'player', 3);
     this.player.anchor.setTo(0.5);
     this.player.animations.add('walking', [0, 1, 2, 1], 6, true);
+    this.game.physics.arcade.enable(this.player);
     this.player.play('walking');
 }
 
